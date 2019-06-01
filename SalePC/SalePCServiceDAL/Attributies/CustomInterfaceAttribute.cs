4d2b6e8a7b1abdaf1c80c2118a18ba360b215b40
XRafe <nikita-gorin98@yandex.ru>
2019-06-01 10:20:41 +0400

    ch

---
+++ SalePC/SalePCServiceDAL/Attributies/CustomInterfaceAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalePCServiceDAL.Attributies
+{
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class CustomInterfaceAttribute : Attribute
+    {
+        public CustomInterfaceAttribute(string descript)
+        {
+            Description = string.Format("Описание инетфейса: ", descript);
+        }
+        public string Description { get; private set; }
+    }
+}
